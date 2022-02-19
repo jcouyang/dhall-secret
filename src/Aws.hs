d9635d72b58d8bb7a294515614cd82fd72229e73
Jichao Ouyang <oyanglulu@gmail.com>
2022-02-19 13:26:28 +1100

    read AWS_REGION to override region

--- src/Aws.hs
+++ src/Aws.hs
@@ -1,12 +1,24 @@
 module Aws where
 
 import           Control.Lens
-import           Network.AWS  (Credentials (Discover), HasEnv (envLogger),
-                               LogLevel (Debug, Info), newEnv, newLogger,
-                               runAWS, runResourceT)
-import           System.IO    (stdout)
+import           Data.Text                (pack)
+import           Network.AWS              (Credentials (Discover),
+                                           HasEnv (envLogger),
+                                           LogLevel (Debug, Info), envRegion,
+                                           newEnv, newLogger, runAWS,
+                                           runResourceT)
+import           Network.AWS.Data         (fromText)
+import           System.Environment.Blank (getEnv)
+import           System.IO                (stdout)
 
 awsRun cmd = do
-  env <- newEnv Discover
   logger <- newLogger Info stdout
-  runResourceT $ runAWS (env & envLogger .~ logger) cmd
+  discover <- newEnv Discover
+  defaultRegion <- getEnv "AWS_REGION"
+  runResourceT $ runAWS (case (hush . fromText . pack) =<< defaultRegion of
+    Nothing     -> discover & envLogger .~ logger
+    Just region -> discover & envRegion .~ region & envLogger .~ logger)
+    cmd
+  where
+    hush (Left _)  = Nothing
+    hush (Right x) = Just x
