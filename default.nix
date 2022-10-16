e692531060b72db86f00deb36824503cc0a96ba7
Jichao Ouyang <oyanglulu@gmail.com>
2022-10-16 23:49:46 +1100

    use hix

--- default.nix
+++
@@ -1,6 +0,0 @@
-{pkgs ? import ./nixpkgs.nix, haskellPackage ? pkgs.haskell.packages.ghc8107 }:
-let dhall-secret-cabal = haskellPackage.callCabal2nix "dhall-secret" ./. {};
-in {
-  dhall-secret = pkgs.haskell.lib.justStaticExecutables dhall-secret-cabal;
-  inherit dhall-secret-cabal;
-}
--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@
   inputs.haskellNix.url = "github:input-output-hk/haskell.nix";
   inputs.nixpkgs.follows = "haskellNix/nixpkgs-unstable";
   inputs.flake-utils.url = "github:numtide/flake-utils";
+
   outputs = { self, nixpkgs, flake-utils, haskellNix }:
     let
       supportedSystems = [
