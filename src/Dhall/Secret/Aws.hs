2f9df3b0730c2a85b34bb03ac8962beedd2ea0d9
Jichao Ouyang <oyanglulu@gmail.com>
2024-12-06 22:43:29 +1100

    update ghc and amazonka

--- src/Dhall/Secret/Aws.hs
+++ src/Dhall/Secret/Aws.hs
@@ -1,24 +1,12 @@
+{-# LANGUAGE FlexibleContexts #-}
 module Dhall.Secret.Aws where
 
-import           Control.Lens             ((&), (.~))
-import           Data.Text                (pack)
-import           Network.AWS              (AWS, Credentials (Discover),
-                                           HasEnv (envLogger), LogLevel (Info),
-                                           envRegion, newEnv, newLogger, runAWS,
-                                           runResourceT)
-import           Network.AWS.Data         (fromText)
-import           System.Environment.Blank (getEnv)
+import Data.Typeable (Typeable)
+import         qualified  Amazonka
 import           System.IO                (stdout)
 
-awsRun :: Network.AWS.AWS b -> IO b
+awsRun ::  (Amazonka.AWSRequest a, Typeable a, Typeable (Amazonka.AWSResponse a)) => a -> IO (Amazonka.AWSResponse a)
 awsRun cmd = do
-  logger <- Network.AWS.newLogger Network.AWS.Info stdout
-  discover <- Network.AWS.newEnv Network.AWS.Discover
-  defaultRegion <- getEnv "AWS_REGION"
-  Network.AWS.runResourceT $ Network.AWS.runAWS (case (hush . fromText . pack) =<< defaultRegion of
-    Nothing     -> discover & Network.AWS.envLogger .~ logger
-    Just region -> discover & Network.AWS.envRegion .~ region & Network.AWS.envLogger .~ logger)
-    cmd
-  where
-    hush (Left _)  = Nothing
-    hush (Right x) = Just x
+  logger <- Amazonka.newLogger Amazonka.Info stdout
+  discover <- Amazonka.newEnv Amazonka.discover
+  Amazonka.runResourceT $ Amazonka.send (discover { Amazonka.logger =logger}) cmd
